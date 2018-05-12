69d4da79e8a0f2a641d71be9d467dfd026381d32
Gregory Mozias <grishamo@gmail.com>
2018-05-12 22:37:53 +0300

    Add project files.

---
+++ B18 Ex04 317612950 321373136/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B18_Ex04_317612950_321373136
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Ex04.Menu.Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex04.Menu.Test
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
